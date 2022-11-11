17c80c428dd82f00b0dfcb8b3839e2fb33c9ab71
thongnguyen02 <thongnq02@gmail.com>
2022-11-11 23:58:34 +0700

    refactor project package

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -9,9 +9,16 @@ import { createTodo } from '../../businessLogic/todos'
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const newTodo: CreateTodoRequest = JSON.parse(event.body)
-    // TODO: Implement creating a new TODO item
+    const userId = getUserId(event)
+    const todoItem = await createTodo(userId, newTodo)
 
-    return undefined
+    return {
+      statusCode: 201,
+      body: JSON.stringify({
+        todoItem
+      })
+    }
+  }
 )
 
 handler.use(
